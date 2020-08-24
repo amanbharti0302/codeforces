421d7763d6fba033ed3223c171eeb5a0f353c6f4
AMAN BHARTI <amanbharti0302@gmail.com>
2020-08-24 14:09:29 +0530

    programme to check whether a graph is bipartite or not

--- utils/bipartite.cpp
+++ utils/bipartite.cpp
@@ -57,9 +57,9 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    // int t;
-    // cin>>t;
-    // while(t-->0)
+    int t;
+    cin>>t;
+    while(t-->0)
     solve();
 
     #ifdef home
